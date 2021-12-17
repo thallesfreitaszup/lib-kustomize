b4c999419ab72ec43671ea1e4eb2125acd303d66
thalles <lopthalles@gmail.com>
2021-12-17 10:25:47 -0300

    add more tests
    Signed-off-by: thalles <lopthalles@gmail.com>

--- cache/cache_test.go
+++ cache/cache_test.go
@@ -37,7 +37,7 @@ var _ = Describe("Cache", func() {
 		})
 	})
 
-	Context("When is the second request for a repository and the content of repository do not changed", func() {
+	Context("When is the second request for a repository and the content of repository did not change", func() {
 		It("should return the cached manifests", func() {
 
 			mockCache.On("Get", source).Return(etag, true)
@@ -51,6 +51,20 @@ var _ = Describe("Cache", func() {
 		})
 	})
 
+	Context("When is a invalid request ", func() {
+		It("should return error", func() {
+			errorRequest := errors.New("error sending request")
+			mockCache.On("Get", source).Return(etag, true)
+			httpClient.On("Do", mock.Anything).Return(GetHTTPResponseWithStatusBadRequest(), errorRequest)
+			mockCache.On("Set", source, etag, int64(1)).Times(1).Return(true)
+			mockCache.On("Get", etag).Return(getManifestsCached(), true)
+			manifestCache := cache.New(mockCache, httpClient)
+			manifests, err := manifestCache.GetManifests(source)
+			assert.Equal(GinkgoT(), err, errorRequest)
+			assert.Equal(GinkgoT(), len(manifests), 0)
+		})
+	})
+
 	Context("When is the second request for a repository and the content of repository changed", func() {
 		It("should not return cached manifests", func() {
 
@@ -61,6 +75,39 @@ var _ = Describe("Cache", func() {
 			manifests, err := manifestCache.GetManifests(source)
 			assert.Equal(GinkgoT(), err, errors.New("resource modified, should download it again"))
 			assert.Equal(GinkgoT(), len(manifests), 0)
+		})
+	})
+
+	Context("when there is no error on cache operations", func() {
+		It("should add manifests to cache successfully", func() {
+
+			mockCache.On("Get", source).Return(etag, true)
+			mockCache.On("Set", etag, getManifestsCached(), int64(1)).Times(1).Return(true)
+			manifestCache := cache.New(mockCache, httpClient)
+			err := manifestCache.Add(source, getManifestsCached())
+			assert.Equal(GinkgoT(), err, nil)
+		})
+	})
+
+	Context("when fails to get a key on cache", func() {
+		It("should return error", func() {
+
+			mockCache.On("Get", source).Return(nil, false)
+			//mockCache.On("Set", etag, getManifestsCached(), int64(1)).Times(1).Return(true)
+			manifestCache := cache.New(mockCache, httpClient)
+			err := manifestCache.Add(source, getManifestsCached())
+			assert.Equal(GinkgoT(), err, errors.New("error getting etag on cache"))
+		})
+	})
+
+	Context("when fails to set a key on cache", func() {
+		It("should return error", func() {
+
+			mockCache.On("Get", source).Return(etag, true)
+			mockCache.On("Set", etag, getManifestsCached(), int64(1)).Times(1).Return(false)
+			manifestCache := cache.New(mockCache, httpClient)
+			err := manifestCache.Add(source, getManifestsCached())
+			assert.Equal(GinkgoT(), err, errors.New("failed to set manifests to cache"))
 		})
 	})
 })
@@ -123,5 +170,11 @@ func getHTTPResponse(etag string) *http.Response {
 	response.Header = make(map[string][]string)
 	response.Header.Set("etag", etag)
 	response.StatusCode = http.StatusOK
+	return response
+}
+
+func GetHTTPResponseWithStatusBadRequest() *http.Response {
+	response := new(http.Response)
+	response.StatusCode = http.StatusBadRequest
 	return response
 }
